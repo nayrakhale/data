601df6386ee3a60d3f6d70ac5a02abf5473212be
nayra khaled <nanokhaledmohammad@gmail.com>
2023-01-25 18:13:28 +0200

    start data project and add files make project

--- data/login.Designer.cs
+++ data/login.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(264, 229);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(682, 444);
+            this.Name = "login";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
