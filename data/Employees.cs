74bcf036f959a2469c1179515bd4fa46daf86033
nayra khaled <nanokhaledmohammad@gmail.com>
2023-01-27 22:09:35 +0200

    string DOB table

--- data/Employees.cs
+++ data/Employees.cs
@@ -166,6 +166,7 @@ namespace EmployeeMgmt1
                     int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString());
                     //int dep
                     string DOB = DOBTb.Value.Date.ToString();
+                    //string DOB table
                     string JDate = JDateTb.Value.Date.ToString();
                     int Salary = Convert.ToInt32(DailySalTb.Text);
                     string Query = "Update EmployeeTb1 set EmpName = '{0}',EmpGen='{1}',EmpDep={2},EmpDOB='{3}',Empjdate='{4}',EmpSal={5} where Empid= {6}";
