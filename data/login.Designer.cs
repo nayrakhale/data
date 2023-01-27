d438ed89a67421be66fd48b6a190098bdf3a45d7
nayra khaled <nanokhaledmohammad@gmail.com>
2023-01-27 21:29:11 +0200

    //message show

--- data/login.Designer.cs
+++ data/login.Designer.cs
@@ -57,6 +57,7 @@ namespace data
                     //message show
 
                     UNameTb.Text = "";
+                    //Uname tale
                     PasswordTb.Text = "";
                 }
             }
