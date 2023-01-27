a8e61bee4b870cc5da9e1eef009fe67ab42aecbf
nayra khaled <nanokhaledmohammad@gmail.com>
2023-01-27 12:57:12 +0200

    write code in cluss with called function

--- data/Functions.cs
+++ data/Functions.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Xml.Linq;
+using System.Runtime.Remoting.Contexts;
 
-namespace data
+namespace EmployeeMgmt1
 {
     internal class Functions
     {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private String ConStr;
+        private object empDb;
+
+        public Functions()
+        {
+            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\EL MOTHEDA\Documents\empDb.mdf"";Integrated Security=True;Connect Timeout=30";
+            Con = new SqlConnection(ConStr);
+            Cmd = new SqlCommand();
+            Cmd.Connection = Con;
+        }
+        public DataTable GetData(string Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query, ConStr);
+            sda.Fill(dt);
+            return dt;
+        }
+        public int SetData(String Query)
+        {
+            int cnt = 0;
+            if (Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
+            }
+            Cmd.CommandText = Query;
+            cnt = Cmd.ExecuteNonQuery();
+            return cnt;
+        }
     }
 }
