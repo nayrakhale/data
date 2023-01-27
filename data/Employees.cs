4fd21f8be2a775ae51b97df31099c96d7f302a8f
nayra khaled <nanokhaledmohammad@gmail.com>
2023-01-27 21:59:51 +0200

    remove code

--- data/Employees.cs
+++ data/Employees.cs
@@ -94,7 +94,7 @@ namespace EmployeeMgmt1
                     //string DOB
                     string JDate = JDateTb.Value.ToString();
                     //string JDate
-                    int Salary = Convert.ToInt32(DailySalTb.Text);
+                   
                     
                     string Query = "insert into EmployeeTb1 values('{0}','{1}',{2},'{3}','{4}',{5})";
                     Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary);
