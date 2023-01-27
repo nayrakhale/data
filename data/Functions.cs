4a5cbcbc4c360b302f6377eaa4ebf0a018595c61
nayra khaled <nanokhaledmohammad@gmail.com>
2023-01-27 20:25:20 +0200

    fix errro

--- data/Functions.cs
+++ data/Functions.cs
@@ -6,10 +6,6 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.VisualBasic.ApplicationServices;
-using System.Xml.Linq;
-using System.Runtime.Remoting.Contexts;
-using System.Security.Cryptography;
 
 namespace EmployeeMgmt1
 {
@@ -20,11 +16,9 @@ namespace EmployeeMgmt1
         private DataTable dt;
         private SqlDataAdapter sda;
         private String ConStr;
-        private object empDb;
-
         public Functions()
         {
-            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\EL MOTHEDA\Documents\empDb.mdf"";Integrated Security=True;Connect Timeout=30";
+            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\ENG-Mohamed Hassan\Documents\EmpDb.mdf"";Integrated Security=True;Connect Timeout=30";
             Con = new SqlConnection(ConStr);
             Cmd = new SqlCommand();
             Cmd.Connection = Con;
