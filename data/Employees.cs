92a1c42b1aac6449cfe9a7b5f38a5ff0d00b89fd
nayra khaled <nanokhaledmohammad@gmail.com>
2023-01-27 22:44:45 +0200

    button2_Click

--- data/Employees.cs
+++ data/Employees.cs
@@ -68,7 +68,7 @@ namespace EmployeeMgmt1
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            //button2_Click
         }
 
         private void GetDepartment()
